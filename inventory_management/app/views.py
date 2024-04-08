b7141d1f1c7b0409689be2198501116d9ce1f44d
Ahnmad Nadeem <ahmadnadeemqureshi@hotmail.com>
2024-04-08 17:00:46 +0200

    Third Commit

--- inventory_management/app/views.py
+++ inventory_management/app/views.py
@@ -1,23 +1,3 @@
-from django.shortcuts import render
-#from django.http import HttpResponseRedirect
-
-# Create your views here.
-def home(request):
-    pass
-
-
-def add_product(request):
-    pass
-
-
-def update_product(request):
-    pass
-
-
-def delete_product(request):
-    pass
-#-------------------------------------------------
-
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Product, Supplier
